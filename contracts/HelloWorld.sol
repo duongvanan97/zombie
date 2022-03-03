9f62cf3e6bef29a2d853f2272b3a4d3ec23a0a5b
Duong Van An <duongvanan@Duongs-MacBook-Pro.local>
2022-03-03 22:29:57 +0700

    first commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+  string message;
+
+  constructor() public {
+    message = "Hello World";
+  }
+
+  function SayHello() public returns(string memory){
+    return message;
+  }
+}
